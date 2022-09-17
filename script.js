135fb4b1e2652d73663782d44b06127579d39f7b
punchpnp <101230476+punchpnp@users.noreply.github.com>
2022-09-17 18:31:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+
+// สำหรับสุ่มขนาด hole
+hole.addEventListener('animationiteration', () => {
+    var random = -((Math.random() * 300) + 150);
+    hole.style.top = random + "px";
+    counter++;
+});
+
+// ชนะได้แต้ม แพ้แสดง game over
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping == 0){
+        character.style.top = (characterTop + 3)+"px";
+    }
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500 - characterTop);
+    // ถ้า game over 
+    if((characterTop > 480 ) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop +130)))){
+        alert("Game over!!! Score : "+(counter - 1));
+        character.style.top = 100 + "px";
+        counter = 0;
+    }
+    
+}, 10);
+
+// กระโดดเมื่อ click
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        // กระโดดขึ้น
+        if((characterTop > 6) && (jumpCount < 15)){
+            character.style.top = (characterTop - 5)+"px";
+        }
+        // กระโดดลง
+        if(jumpCount > 20){
+            clearInterval(jumpInterval);
+            jumping = 0;
+            jumpCount = 0;
+        }
+        jumpCount++;
+    }, 10);
+}
